225dcb8dd166397f3caa79414a76901fd23bf404
Shogo Matsumoto <sougyo2001@gmail.com>
2016-05-17 21:43:15 +0900

    implement string literal

--- min_parser.hs
+++ min_parser.hs
@@ -16,6 +16,7 @@ data OperatorType =
 
 data Token =   Constant String
              | Identifier String
+             | StringLiteral String
              | Operator OperatorType
   deriving (Show, Eq)
 
@@ -32,6 +33,7 @@ token_parser = many1 $ PosToken <$> getPosition <*> makeToken <* spaces
   where
     makeToken =     try (Constant <$> many1 digit)
                 <|> try identifier
+                <|> try (StringLiteral <$> string_literal)
                 <|> Operator <$> operator
     operator  =     char '+' *> return Plus
                 <|> char '-' *> return Minus
@@ -42,21 +44,39 @@ token_parser = many1 $ PosToken <$> getPosition <*> makeToken <* spaces
     identifier = fmap Identifier identifier_str
     identifier_str = (:) <$> nondigit <*> many (nondigit <|> digit)
     nondigit = letter <|> char '_'
-
+    string_literal = char '"' *> s_char_sequence <* char '"'
+    s_char_sequence = many1 (s_char <|> escape_sequence)
+    s_char = noneOf "\n\"\\"
+    escape_sequence =     try (string "\\'"  *> return '\'')
+                      <|> try (string "\\\"" *> return '"' )
+                      <|> try (string "\\a"  *> return '\a')
+                      <|> try (string "\\b"  *> return '\b')
+                      <|> try (string "\\f"  *> return '\f')
+                      <|> try (string "\\n"  *> return '\n')
+                      <|> try (string "\\r"  *> return '\r')
+                      <|> try (string "\\t"  *> return '\t')
+                      <|>      string "\\v"  *> return '\v'
 
 gen_p m = tokenPrim (\c -> show c) (\pos c _cs -> get_pos c) $ m . get_token
-p_const = gen_p $ \n -> case n of
+p_const  = gen_p $ \n -> case n of
               Constant s -> Just s
               _          -> Nothing
-p_ident = gen_p $ \n -> case n of
+p_ident  = gen_p $ \n -> case n of
               Identifier s -> Just s
               _            -> Nothing
-p_op p  = gen_p $ \n -> if n == Operator p
+p_string = gen_p $ \n -> case n of
+              StringLiteral s -> Just s
+              _               -> Nothing
+p_op p   = gen_p $ \n -> if n == Operator p
               then Just p
               else Nothing
 
 
-data Expr = EIdent String | EConst String | BinOp OperatorType Expr Expr | UnaryOp OperatorType Expr
+data Expr =   EIdent  String
+            | EConst  String
+            | EString String
+            | BinOp OperatorType Expr Expr
+            | UnaryOp OperatorType Expr
   deriving (Show, Eq)
 
 
@@ -71,18 +91,19 @@ term = chainl1 factor $ mul_op <|> div_op
     div_op = p_op Slash    *> return (BinOp Slash)
 
 factor =     try parenthes_op
-         <|> try plus_op
-         <|> try minus_op
+         <|> try unary_plus
+         <|> try unary_minus 
          <|> leaf
   where
     parenthes_op = p_op LeftParenthes *> expr <* p_op RightParenthes 
-    plus_op      = UnaryOp <$> p_op Plus  <*> factor
-    minus_op     = UnaryOp <$> p_op Minus <*> factor
+    unary_plus   = UnaryOp <$> p_op Plus  <*> factor
+    unary_minus  = UnaryOp <$> p_op Minus <*> factor
 
-leaf =     try e_const <|> e_ident
+leaf =     try e_const <|> try e_ident <|> e_string
   where
-    e_const = p_const >>= return . EConst
-    e_ident = p_ident >>= return . EIdent
+    e_const  = p_const  >>= return . EConst
+    e_ident  = p_ident  >>= return . EIdent
+    e_string = p_string >>= return . EString
 
 min_parser = expr <* eof
 
