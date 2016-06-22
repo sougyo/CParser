926cd55d4182cf2f5e9f6766600897435a26598e
Shogo Matsumoto <sougyo2001@gmail.com>
2016-06-22 22:56:59 +0900

    start to implement translation unit

--- min_parser.hs
+++ min_parser.hs
@@ -9,6 +9,8 @@ data OperatorType =
   | RightParenthes 
   | LeftBrace
   | RightBrace
+  | LeftBracket
+  | RightBracket
   | Plus
   | Minus
   | Asterisk
@@ -46,6 +48,7 @@ data OperatorType =
   | HatEqual
   | OrEqual
   | Comma
+  | ThreeDots
   deriving (Show, Eq)
 
 data KeywordType = 
@@ -79,7 +82,8 @@ token_parser = many1 $ PosToken <$> getPosition <*> makeToken <* spaces
                 <|> try keyword_or_identifier
                 <|> try (StringLiteral <$> string_literal)
                 <|> Operator <$> operator
-    operator  =     try (string "++"  *> return PlusPlus)
+    operator  =     try (string "..." *> return ThreeDots)
+                <|> try (string "++"  *> return PlusPlus)
                 <|> try (string "--"  *> return MinusMinus)
                 <|> try (string "<<"  *> return LeftShift)
                 <|> try (string ">>"  *> return RightShift)
@@ -117,15 +121,15 @@ token_parser = many1 $ PosToken <$> getPosition <*> makeToken <* spaces
                 <|> char ')' *> return RightParenthes
                 <|> char '{' *> return LeftBrace
                 <|> char '}' *> return RightBrace
+                <|> char '[' *> return LeftBracket
+                <|> char ']' *> return RightBracket
                 <|> char ',' *> return Comma
 
-keyword_or_identifier = fmap choose identifier
-  where choose s = case lookup s keyword_dictionary of
-                     Just k  -> Keyword    k
-                     Nothing -> Identifier s
-
-identifier = identifier_str
+keyword_or_identifier = fmap choose identifier_str
   where
+    choose s = case lookup s keyword_dictionary of
+                 Just k  -> Keyword    k
+                 Nothing -> Identifier s
     identifier_str = (:) <$> nondigit <*> many (nondigit <|> digit)
     nondigit = letter <|> char '_'
 
@@ -159,7 +163,10 @@ p_op p   = gen_p $ \n -> if n == Operator p
 p_kwd p  = gen_p $ \n -> if n == Keyword p
               then Just p
               else Nothing
-
+p_ident_s s = gen_p $ \n -> case n of
+                Identifier x -> if x == s then Just x else Nothing
+                _            -> Nothing
+ 
 data Expr =   EIdent  String
             | EConst  String
             | EString String
@@ -266,9 +273,7 @@ data Statement =   ExprStatement Expr
 statement = try selection_stmt <|> try block_stmt <|> expr_stmt
 
 selection_stmt = do p_kwd If
-                    p_op LeftParenthes
-                    e <- expr
-                    p_op RightParenthes
+                    e  <- p_op LeftParenthes *> expr <* p_op RightParenthes
                     s1 <- statement
                     s2 <- option NullStatement (p_kwd Else *> statement)
                     return $ IfStatement e s1 s2
@@ -278,6 +283,73 @@ block_stmt = p_op LeftBrace *> block_items <* p_op RightBrace
 block_items = fmap BlkStatement $ many (StatementBlockItem <$> statement)
 
 expr_stmt = fmap ExprStatement (option NullExpr expr <* p_op Semicolon)
+
+declaration = do declaration_spec
+                 sepBy1 init_declarator (p_op Comma)
+                 p_op Semicolon
+
+declaration_spec = many1 $
+  (try type_spec <|> try type_qualifier <|> storage_class_spec)
+
+type_spec =     try (p_ident_s "void")
+            <|> try (p_ident_s "char")
+            <|> try (p_ident_s "short")
+            <|> try (p_ident_s "int")
+            <|> try (p_ident_s "long")
+            <|> try (p_ident_s "float")
+            <|> try (p_ident_s "double")
+            <|> try (p_ident_s "signed")
+            <|> p_ident_s "unsigned"
+
+type_qualifier =     try (p_ident_s "const")
+                 <|> try (p_ident_s "restrict")
+                 <|> try (p_ident_s "volatile")
+
+storage_class_spec =     try (p_ident_s "typedef")
+                     <|> try (p_ident_s "extern")
+                     <|> try (p_ident_s "static")
+                     <|> try (p_ident_s "auto")
+                     <|> p_ident_s "register"
+
+type_qualifier_list = many1 type_qualifier
+
+pointer = many1 $ p_op Asterisk *> many type_qualifier
+
+init_declarator = declarator
+
+declarator = optional pointer *> direct_declarator
+
+direct_declarator = do ident_or_decl >>= rest
+  where
+    ident_or_decl = try p_ident <|> p_op LeftParenthes *> declarator <* p_op RightParenthes
+    rest p   = try (helper p >>= rest) <|> return p
+    helper p =     try (p_op LeftBracket *> (option NullExpr assignment_e) *> p_op RightBracket *> return p)
+               <|> try (p_op LeftParenthes *> parameter_type_list *> p_op RightParenthes *> return p)
+               <|> try (p_op LeftParenthes *> opt_ident_list *> p_op RightParenthes *> return p)
+    opt_ident_list = sepBy p_ident (p_op Comma)
+    
+
+parameter_type_list = do p <- parameter_list
+                         try (parameter_list *> (optional $ p_op Comma *> p_op ThreeDots) *> return p)
+                           <|> return p
+                         
+
+parameter_list = sepBy1 parameter_declaration (p_op Comma)
+
+parameter_declaration = declaration_spec *> declarator
+
+initializer = assignment_e
+
+function_definition = do
+  declaration_spec
+  declarator
+  many declaration
+  block_stmt
+
+external_declaration = try (function_definition *> return ()) <|> (declaration *> return ())
+
+translation_unit = many1 external_declaration
+
 
 min_parser = statement <* eof
 
